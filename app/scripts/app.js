edf14ff59dac257e8a56d3e53b5a7a3445ae9387
Mayur Patel <mayurt20@gmail.com>
2016-08-26 17:57:18 +0530

    adds validation to forms

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -69,11 +69,26 @@ angular.module('confusionApp',[])
   }])
 
   .controller('ContactController', ['$scope', function($scope) {
-
-    $scope.feedback = {mychannel:"", firstName:"", lastName:"",
-                       agree:false, email:"" };
+    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+    $scope.channels = channels;
+    $scope.invalidChannelSelection = false;
   }])
 
   .controller('FeedbackController', ['$scope', function($scope) {
+    $scope.sendFeedback = function() {
+      console.log($scope.feedback);
+      if ($scope.feedback.agree && ($scope.feedback.mychannel === "")&& !$scope.feedback.mychannel) {
+        $scope.invalidChannelSelection = true;
+        console.log('incorrect');
+      } else {
+        $scope.invalidChannelSelection = false;
+        $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                           agree:false, email:"" };
+        $scope.feedback.mychannel="";
 
+        $scope.feedbackForm.$setPristine();
+        console.log($scope.feedback);
+      }
+    };
   }]);
